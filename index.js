379eba67e4d2bba9dbf96520f98190ae75091969
StVitaliy <64489694+StVitaliy@users.noreply.github.com>
2020-05-09 16:28:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-
-
 const fs=require('fs')
 const http=require('http');
 const port=3000;
-let page = fs.readFileSync('index.html', 'utf8');
 
+let page = fs.readFileSync('index.html', 'utf8');
 http.createServer(function(req,res){
   if(req.url==='/'){
-    res.write('Hello world');
-        res.end();
+   res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Hello world');
   }else if(req.url==='/contact'){
     
-        res.end(page);
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(page);
+
 }else if(req.url==='/about'){
- console.log('вы запросили страницу localhost:3000/about')
-      res.end();
-}  else{
-  res.write('неправильный вариант ввода');
-        res.end();
+   console.log('вы запросили страницу localhost:3000/about')
+ res.end()
+}else{
+  res.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'});
+  res.end('вы ввели не правильный адрес http/')
 }
      }).listen(port,function(){
     console.log('сервер загрузился port= '+port);
